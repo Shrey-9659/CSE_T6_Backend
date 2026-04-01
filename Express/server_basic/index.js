c8a9a19913eb89b3683058c9d3d779813a7df93a
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2026-04-01 09:57:03 +0530

    Dynamic Routing - Lecture

--- Express/server_basic/index.js
+++ Express/server_basic/index.js
@@ -1,31 +1,61 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
+const users = require("./users.json");
 
-
-const app = express()
+const app = express();
 
 // serverName.methodName("/route", callback function)
 app.get("/", (req, res) => {
-    // To send data
-    res.send("I am on Home Page")
-})
+  // To send data
+  res.send("I am on Home Page");
+});
 
-const aboutFileLocation = path.join(__dirname, "about.html")
+const aboutFileLocation = path.join(__dirname, "about.html");
 
 app.get("/about", (req, res) => {
-    // res.sendFile("/Users/shreykhandelwal/Desktop/Shoolini/Sem 2/AI_T6/Express/server_basic/about.html")
-    res.sendFile(aboutFileLocation)
-})
+  // res.sendFile("/Users/shreykhandelwal/Desktop/Shoolini/Sem 2/AI_T6/Express/server_basic/about.html")
+  res.sendFile(aboutFileLocation);
+});
 
 app.get("/user", (req, res) => {
+  res.json({
+    name: "Shrey",
+    university: "Shoolini",
+    profession: "Trainer",
+    year: 2026,
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+// Dynamic Routing
+app.get("/api/users/:id", (req, res) => {
+  // const reqId = +(req.params.id)
+  // const reqId = parseInt(req.params.id)
+  const reqId = Number(req.params.id);
+  // console.log(reqId)
+  if (isNaN(reqId)) {
     res.json({
-        name : "Shrey",
-        university : "Shoolini",
-        profession : "Trainer",
-        year : 2026
-    })
-})
+      success: false,
+      msg: "Please enter valid user id",
+    });
+  } else {
+    const providedUser = users.find((user) => {
+      return reqId == user.id;
+    });
+    if (!providedUser) {
+      res.json({
+        success: false,
+        msg: "User not found with the provided id",
+      });
+    } else {
+      res.json(providedUser);
+    }
+  }
+});
 
 app.listen(3000, () => {
-    console.log("Server started...")
-})
+  console.log("Server started...");
+});
